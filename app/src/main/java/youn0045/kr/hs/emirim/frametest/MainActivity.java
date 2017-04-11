8bf351c7676dad61d59f32ac7acd4ed730ed579a
forelise25 <forelise25@naver.com>
2017-04-11 20:20:28 +0900

    #1 MakeLayout

---
+++ app/src/main/java/youn0045/kr/hs/emirim/frametest/MainActivity.java
@@ -0,0 +1,16 @@
+package youn0045.kr.hs.emirim.frametest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button but1= findViewById()
+    }
+}
